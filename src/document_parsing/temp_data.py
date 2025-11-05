c58223e66879b51fe3b6381cc2e245d0c9f9a3f1
Mamoon018 <mamonhaider11@gmail.com>
2025-11-05 21:18:58 +0100

    Added context extractor based on the page size context window, and made it precise, removed code repititions, flexibility to choose number of units around the current unit increased substatially

--- src/document_parsing/temp_data.py
+++ src/document_parsing/temp_data.py
@@ -1,6 +1,58 @@
 
 
+"""
+Minima: (When current chunk has index 0)
+
+    Case-1: Index = 0
+    Solution: (Assuming title type exception)
+        (a) Extract two chunks from previous page. (Done)
+        (b) Extract next two chunks from current page (Done)
 
 
+Maxima: (When current chunk has index among top two highest indexes of page such as if higest index is 17. Maxima zone 17.)
+    
+    Case-1: Index = 17
+    Solution: (Assuming title type exception
+        (a) Extract two chunks from next page. (Done)
+        (b) Extract previous two chunks from current page (Done) 
+
+        
+
+        
+Current page Issue:
+
+    Case-1: Index = 1   (NOT HANDLED)
+    Solution: (Assuming title type exception)
+        (a) Extract next two chunks from the current page (Done)
+        (b) Extract previous two chunks from the current page (Done)      (There is only 1 chunk left)
 
 
+Current page Issue:
+    Case-2: Index = 16   (NOT HANDLED)
+    Solution: (Assuming title type exception)
+        (a) Extract next two chunks from the current page (Done)          (There is only 1 chunk left)
+        (b) Extract previous two chunks from the current page (Done)  
+
+        
+Solution questions:
+1- How to find out that previous unit is the last unit of the current page?
+2- If we are able to find then we can associate the "break" with it in order to ensure it does not run loop anymore.
+        
+
+"""
+
+
+                                        ### APPROACH NO. 2 ###
+"""
+
+1- Find out the page of current chunk. Using that, find out previous page and next page.
+2- Access all chunks of current page, extract their index numbers and store in a list in their hierarchical order.
+3- Access all chunks of the next page, and of the previous page. Extract their index numbers and store separately.
+4- Put them together in a single list 
+5- Fetch the next two & previous two chunks of the current chunk from this list.
+    (a) loop over the list to find out the current chunk
+    (b) Findout the index of the current chunk in the list
+    (c) Fetch the previous two chunks - if text or title then add, under title break the loop else add another chunk. 
+    (e) Fetch the next two chunks - if text then add else break the loop.
+
+"""
--- src/utils.py
+++ src/utils.py
@@ -134,21 +134,20 @@ class context_extractor():
         
         # get the surrounding chunks from the combined atomic units dict
         """
-        Surrounding chunks:
+        Surrounding chunks: (For Multi-model content, it makes sense to fetch the two next & two previous chunks. As
+        we usually give description of multi-model content after it's display and some context before its display.)
         0- Initialize the list to store the contextual (Done)
         1- Use the current unit info details to get the current page number (Done)
         2- Calculate the previouse page number and the next page number (Done)
         3- Get the list of the number of indexes on the current page (Done)
-        4- check if index of the current unit is minima = (minim or minim + 1) or maxima = (maximum - 1) index of the page (Done)
-        5- If current chunk neither lies in minima nor maxima index of the current page then just take two next and two previouse chunks (Done)
-        6- If lies in minima zone, then filter out the units of previous page.
-        7- fetch the last two units of previous page
-        8- If lies in maxima zone, then filter out the units of next page
-        9- fetch the first two units of next page
-        In this way, we will be able to get the surrounding units required to extract the context 
+        4- check if index of the current unit is in minima zone = (minim or minim + 1) or maxima = (maximum - 1) zone index of the page (Done)
+        5- If current chunk neither lies in minima nor maxima index of the current page then just take two next and two previouse chunks of current page (Done)
+        6- If lies in minima zone, then filter out the units of previous page. (Done)
+        7- fetch the last two units of previous page (Done)
+        8- If lies in maxima zone, then filter out the units of next page (Done)
+        9- fetch the first two units of next page (Done)
+        In this way, we will be able to get the surrounding units required to extract the context (Done)
         """
-
-        list_chunks_for_context_extraction = []
 
         current_page = page_of_chunk
         previous_page = page_of_chunk - 1
@@ -167,7 +166,7 @@ class context_extractor():
 
         maximum_index_of_page = index_of_current_page
         minima = lowest_index_of_page + 1
-        maxima_zone = maximum_index_of_page - 2
+        maxima_zone = maximum_index_of_page - 1
 
         # Based on the placement of the current chunk, we will extract the surrounding chunks
         if index_of_current_chunk > maxima_zone:
@@ -185,77 +184,179 @@ class context_extractor():
         list_of_context_chunks = []
         
         if need_to_extract_from_next_page:
-            # check how many chunks do we have before the next heading starts on the next page
+
+            # get two previous chunks from current page
+            count = 1
+            index_previous_chunk_of_CP = index_of_current_chunk - count
+            index_previous_chunk_of_CP = max(0,index_previous_chunk_of_CP)
+            for unit_of_CP in units_of_current_page:
+                if unit_of_CP["index_on_page"] == index_previous_chunk_of_CP:
+                    count =+ 1
+                    if unit_of_CP["content_type"] == "text" and count <= 3:
+                        previous_chunk_of_CP = unit_of_CP.get("raw_content","")
+                        list_of_context_chunks.append(previous_chunk_of_CP)
+                    elif unit_of_CP["content_type"] == "title" and count < 3: 
+                        count =+ 1                                              # if found title in previous chunk, then we need to chunk behind that title as it would belong to previous heading.
+                        previous_chunk_of_CP = unit_of_CP.get("raw_content","")
+                        list_of_context_chunks.append(previous_chunk_of_CP)      
+                    else:
+                        break
+
+
+            # Get two chunks of the next page
             units_of_next_page = [units for units in combined_atomic_units if units["page_no."] == next_page]
-            units_of_next_page = sorted(units_of_next_page, key= lambda x:x["index_on_page"], reverse=True)
             """
-            1- Loop over the units of the next page
-            2- if content is "text", add it to the list of context chunks
-            3- else terminate the loop and end the appending of chunks to the list of context chunks
+            1- Extract first two chunks of the next page only if they are text - otherwise, break the loop.
             """
             count = 0
-            for unit_of_NP in units_of_next_page and count < 3:
+            for unit_of_NP in units_of_next_page:
                 count =+ 1
-                if unit_of_NP["content_type"] == "text":
+                if unit_of_NP["content_type"] == "text" and count < 3:
                     chunk_of_NP = unit_of_NP.get("raw_content","")
                     list_of_context_chunks.append(chunk_of_NP)
                 else:
                     break
 
-            # check how many chunks do we have to before the previous heading on the previous page
+            # Minima Zone: fetch maximum two chunks from previous page before heading, and next two chunks of current page before heading.
         if need_to_extract_from_previous_page:
             units_of_previous_page = [units for units in combined_atomic_units if units["page_no."] == previous_page]
             units_of_previous_page = sorted(units_of_previous_page, key=lambda x:x["index_on_page"], reverse=True)
-
+            
+                        # Two chunks from previous page
+            count = 0
             for unit_of_PP in units_of_previous_page:
-                count = 0
+                count =+ 1
                 if unit_of_PP["content_type"] == "text" and count < 3:
+                    chunk_of_PP = unit_of_PP.get("raw_content","")
+                    list_of_context_chunks.append(chunk_of_PP)
+                elif unit_of_PP["content_type"] == "title" and count < 3:
                     count =+ 1
                     chunk_of_PP = unit_of_PP.get("raw_content","")
                     list_of_context_chunks.append(chunk_of_PP)
                 else:
                     break
-        
-        if need_to_extract_from_current_page:
-            # We have already calculated units for current page
-            """
-            1- Using the index of current chunk, we will find out previouse two chunks & next two chunks.
-            2- iterating_chunk = 0
-            3- Inside the loop, for every text chunk, update iterating_chunk = ICC - count 
-            4- When loop reaches to the previous chunk, count =+1 (means one previous chunk checked) & check its content type, if text then extract chunk. 
-            5- If content_type is "title", then we will add count =+ 1 again, because we only want to extract main heading as para before that heading will
-            be most likely irrelevant. 
-            """
-            # Previous chunks of current page
-            count = 1
-            index_previous_chunk_of_CP = index_of_current_chunk - count
-            for unit_of_CP in units_of_current_page:
-                if unit_of_CP["index_on_page"] == index_previous_chunk_of_CP:
-                    count =+ 1
-                    if unit_of_CP["content_type"] == "text" and count < 3:
-                        previous_chunk_of_CP = unit_of_CP.get("raw_content","")
-                        list_chunks_for_context_extraction.append(previous_chunk_of_CP)
-                    elif unit_of_CP["content_type"] == "title" and count < 3:
-                        count =+ 1
-                        previous_chunk_of_CP = unit_of_CP.get("raw_content","")
-                        list_chunks_for_context_extraction.append(previous_chunk_of_CP)      
-                    else:
-                        break
-            # Next chunks of current page
+                        # Next two chunks from current page
             count = 1
             index_next_chunk_of_CP = index_of_current_chunk + count
             for unit_of_CP in units_of_current_page:
                 if unit_of_CP["index_on_page"] == index_next_chunk_of_CP:
                     count =+ 1
-                    if unit_of_CP["content_type"] == "text" and count < 3:
+                    if unit_of_CP["content_type"] == "text" and count <= 3:
                         next_chunk_of_CP = unit_of_CP.get("raw_content","")
-                        list_chunks_for_context_extraction.append(next_chunk_of_CP)                
+                        list_of_context_chunks.append(next_chunk_of_CP)                
+                    else:
+                        break
+
+        
+        if need_to_extract_from_current_page:
+            # We have already calculated units for current page
+
+            # Previous chunks of current page
+            """
+            1- Case_no.1: If title type is coming first, and as previous chunk then pick title type considering it reference for figure and terminate the loop.
+            2- Case_no.2: If text type is coming first, & then title type as previous chunks then pick both and terminate the loop.
+            3- Case_no.3: If text type is coming as 2 times as previous chunks then pick both and terminate the loop.
+            """
+            count = 1
+            index_previous_chunk_of_CP = index_of_current_chunk - count
+            index_previous_chunk_of_CP = max(0,index_previous_chunk_of_CP)
+            for unit_of_CP in units_of_current_page:
+                if unit_of_CP["index_on_page"] == index_previous_chunk_of_CP:
+                    
+                    count =+ 1
+                    if unit_of_CP["content_type"] == "text" and count <= 3:
+                        previous_chunk_of_CP = unit_of_CP.get("raw_content","")
+                        list_of_context_chunks.append(previous_chunk_of_CP)
+                    elif unit_of_CP["content_type"] == "title" and count < 3: 
+                        count =+ 1                                              # if found title in previous chunk, then we need to chunk behind that title as it would belong to previous heading.
+                        previous_chunk_of_CP = unit_of_CP.get("raw_content","")
+                        list_of_context_chunks.append(previous_chunk_of_CP)      
+                    else:
+                        break
+            # Next chunks of current page
+            """
+            1- Case_no.1: If text type chunks are coming as next two chunks, then pick both and terminate the loop.
+            2- Case_no.2: If there is any chunk other than text type, then terminate the loop.
+            """
+            count = 1
+            index_next_chunk_of_CP = index_of_current_chunk + count
+            for unit_of_CP in units_of_current_page:
+                if unit_of_CP["index_on_page"] == index_next_chunk_of_CP:
+                    count =+ 1
+                    if unit_of_CP["content_type"] == "text" and count <= 3: 
+                        next_chunk_of_CP = unit_of_CP.get("raw_content","")
+                        list_of_context_chunks.append(next_chunk_of_CP)
+                    # We do not need to catch next heading coming after current chunk - it will most likely to be starting another topic
+                    # and irrelevant to previous figure. So, if next-chunk is not "text" then terminate loop. 
                     else:
                         break
 
             
         
         return print(list_of_context_chunks)
+    
+    def multi_model_extractor(self,current_multi_model_unit:list[dict[str]]):
+        """
+        It takes the current unit, and fetches its placement details to identify the surrouding chunks in the documents and then 
+        place them in the chunks for context extraction list.
+
+        Here is the workflow:
+        1- Find out the page of current chunk. Using that, find out previous page and next page. (Done)
+        2- Access all chunks of current page, extract their index numbers and store in a list in their hierarchical order. (Done)
+        3- Access all chunks of the next page, and of the previous page. Extract their index numbers and store separately. (Done)
+        4- Put them together in a single list (Done)
+        5- Fetch the next two & previous two chunks of the current chunk from this list.
+            (a) loop over the list to find out the current chunk (Done)
+            (b) Findout the index of the current chunk in the list (Done)
+            (c) Fetch the previous two chunks - if text or title then add, under title break the loop else add another chunk. (In-progress)
+            (e) Fetch the next two chunks - if text then add else break the loop.
+                
+        """
+
+        # Input variables
+        all_knowledge_units = self.all_knowledge_units
+        current_chunk = current_multimodel_unit
+
+        # As it is list of chunk because of figure & caption unit so, we need to find out only figure unit as reference for placement of current chunk
+        chunk_of_figure = None
+        # Placement details of the current chunk
+        for chunk in current_chunk:
+            if "table_image_path" in chunk:
+                page_of_current_chunk = chunk.get("page_no.","")
+                page_index_of_current_chunk = chunk.get("index_on_page","")
+                content_of_current_chunk = chunk.get("table_image_path","")
+                chunk_of_figure = chunk
+        
+        surrounding_pages_chunks = []
+
+        # Previous page & Next page 
+        previous_page = page_of_current_chunk - 1
+        next_page = page_of_current_chunk + 1
+        pages_relvant_for_context = [previous_page,page_of_current_chunk,next_page]
+        # Fetch all the chunks from previous page, current page, and next page (In this hierarchical order)
+        for page in pages_relvant_for_context:
+            for chunks in combined_knowledge_units:
+                if chunks.get("page_no.") == page:
+                    surrounding_pages_chunks.append(chunks)
+
+        # Fetch the index of the current chunk in the list of surrounding chunks
+        index_of_current_chunk = surrounding_pages_chunks.index(chunk_of_figure)
+        
+        # Fetch previous two chunks
+        index_of_previous_chunk_1 = index_of_current_chunk - 1
+        index_of_previous__chunk_2 = index_of_current_chunk - 2
+        indexes_of_previous_chunks = [index_of_previous_chunk_1,index_of_previous__chunk_2]
+        list_of_previous_chunks = [surrounding_pages_chunks[i] for i in indexes_of_previous_chunks]
+
+        # Fetch next two chunks 
+        index_of_next_chunk_1 = index_of_current_chunk + 1
+        index_of_next__chunk_2 = index_of_current_chunk + 2
+        indexes_of_next_chunks = [index_of_next_chunk_1,index_of_next__chunk_2]
+        list_of_next_chunks = [surrounding_pages_chunks[i] for i in indexes_of_next_chunks]
+
+
+
+        return print(list_of_next_chunks, surrounding_pages_chunks)
 
 
 if __name__ == "__main__":
@@ -263,4 +364,4 @@ if __name__ == "__main__":
     multi_model_knowledge_units, textual_knowledge_units = units_splitter(knowledge_units_list=combined_knowledge_units)
 
     extractor = context_extractor(all_knowledge_units=combined_knowledge_units)
-    extractor.multi_model_context_extractor(multi_model_knowledge_units=multi_model_knowledge_units)
+    extractor.multi_model_extractor(current_multi_model_unit=current_multimodel_unit)
